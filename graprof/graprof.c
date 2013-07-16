aeb7e0a59822bdb70ac590663c301f516a2a8cb7
Andreas Grapentin <andreas@grapentin.org>
2013-07-16 18:38:40 +0200

    added analysis infrastructure

--- graprof/graprof.c
+++ graprof/graprof.c
@@ -11,7 +11,7 @@ extern int yylex(const char *filename);
 int 
 main (int argc, char *argv[])
 {
-  struct arguments args = { 0, 0 };
+  struct arguments args = { 0, 0, 0, 0 };
   argp_parse (&argp, argc, argv, 0, 0, &args);
 
   yyin = fopen(args.trace_filename, "r");
@@ -19,6 +19,40 @@ main (int argc, char *argv[])
   
   int res = yylex(args.trace_filename);
   feedback_assert(!res, "%s", args.trace_filename);
+
+  FILE *out = stdout;
+  if (args.out_filename && (args.tasks & ~GRAPROF_NO_GUI))
+    {
+      out = fopen(args.out_filename, "w");
+      feedback_assert(out, "%s", args.out_filename);
+    }
+
+  if (args.tasks & GRAPROF_FLAT_PROFILE)
+    {
+      errno = ENOSYS;
+      feedback_assert_wrn(0, "--flat-profile");
+    }
+
+  if (args.tasks & GRAPROF_CALL_TREE)
+    {
+      errno = ENOSYS;
+      feedback_assert_wrn(0, "--call-tree");
+    }
+
+  if (args.tasks & GRAPROF_MEMORY_PROFILE)
+    {
+      errno = ENOSYS;
+      feedback_assert_wrn(0, "--memory-profile");
+    }
+
+  if (out == stdout)
+    fclose(out);
+
+  if (!(args.tasks & GRAPROF_NO_GUI))
+    {
+      errno = ENOSYS;
+      feedback_assert_wrn(0, "tracing gui");
+    }
 
   return 0;
 }
