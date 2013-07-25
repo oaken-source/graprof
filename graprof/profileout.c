48e3006c1ad8384779b94c67fe13e141a2d28275
Andreas Grapentin <andreas@grapentin.org>
2013-07-25 22:11:05 +0200

    sorting flat profile output - kind of fixes time readability issue

--- graprof/profileout.c
+++ graprof/profileout.c
@@ -5,6 +5,7 @@
 #include "trace.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <inttypes.h>
 
@@ -35,6 +36,17 @@ profileout_format_time (unsigned long long *time, const char **prefix)
     }
 }
 
+static int
+cmpfunction (const void *p1, const void *p2)
+{
+  function *f1 = *(function**)p1;
+  function *f2 = *(function**)p2;
+
+  if (f2->self_time == f1->self_time)
+    return strcmp(f1->name, f2->name);
+  return f2->self_time - f1->self_time;
+}
+
 int 
 profileout_flat_profile ()
 {
@@ -54,10 +66,16 @@ profileout_flat_profile ()
   unsigned int nfunctions = 0;
   function *functions = function_get_all(&nfunctions);
 
+  function **sorted_functions = malloc(sizeof(function*) * nfunctions);
   unsigned int i;
   for (i = 0; i < nfunctions; ++i)
+    sorted_functions[i] = functions + i;
+
+  qsort(sorted_functions, nfunctions, sizeof(function*), cmpfunction);
+
+  for (i = 0; i < nfunctions; ++i)
   {
-    function *f = functions + i;
+    function *f = sorted_functions[i];
 
     fprintf(graprof_out, "%6.2f ", (100.0 * f->self_time) / trace_get_total_runtime());
 
@@ -83,11 +101,14 @@ profileout_flat_profile ()
 
     fprintf(graprof_out, "%6llu %ss  ", time, prefix);
 
-    fprintf(graprof_out, "%s", f->name);
     if (!strcmp(f->name, "??"))
-      fprintf(graprof_out, " [0x%" PRIxPTR "]", f->address);
-    fprintf(graprof_out, "\n");
+      fprintf(graprof_out, "0x%" PRIxPTR , f->address);
+    else
+      fprintf(graprof_out, "%s", f->name);
+    fprintf(graprof_out, " [%tu]\n", f - functions);
   }
+
+  free(sorted_functions);
 
   fprintf(graprof_out, "\n");
   fprintf(graprof_out, "  %%         the percentage of the total running time of the\n"); 
@@ -97,7 +118,8 @@ profileout_flat_profile ()
   fprintf(graprof_out, "     time   spent below this function in the call tree\n");
   fprintf(graprof_out, "\n");
   fprintf(graprof_out, " self       the absolute total running time spent in this\n");
-  fprintf(graprof_out, "  time      function alone\n");
+  fprintf(graprof_out, "  time      function alone - this is the major sort of this\n");
+  fprintf(graprof_out, "            listing\n");
   fprintf(graprof_out, "\n");
   fprintf(graprof_out, " calls      the number of times this function was invoked\n");
   fprintf(graprof_out, "\n");
@@ -108,7 +130,8 @@ profileout_flat_profile ()
   fprintf(graprof_out, "  /call     in the call tree per call\n");
   fprintf(graprof_out, "\n");
   fprintf(graprof_out, " name       the name of the function, if available, else its\n");
-  fprintf(graprof_out, "            address\n");
+  fprintf(graprof_out, "            address, and a unique id for reference in the call\n");
+  fprintf(graprof_out, "            graph - this is the minor sort of this listing\n");
 
   return 0;
 }
