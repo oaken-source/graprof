66dc70bd4910d7a87adfeb8be85af8b3b7ad59d9
Andreas Grapentin <andreas@grapentin.org>
2013-08-23 21:04:40 +0200

    added license headers to files

--- graprof/graprof.h
+++ graprof/graprof.h
@@ -1,4 +1,24 @@
 
+/******************************************************************************
+ *             graprof - a profiling and trace analysis tool                  *
+ *                                                                            *
+ *    Copyright (C) 2013  Andreas Grapentin                                   *
+ *                                                                            *
+ *    This program is free software: you can redistribute it and/or modify    *
+ *    it under the terms of the GNU General Public License as published by    *
+ *    the Free Software Foundation, either version 3 of the License, or       *
+ *    (at your option) any later version.                                     *
+ *                                                                            *
+ *    This program is distributed in the hope that it will be useful,         *
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of          *
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ *    GNU General Public License for more details.                            *
+ *                                                                            *
+ *    You should have received a copy of the GNU General Public License       *
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.   *
+ ******************************************************************************/
+
+
 #pragma once
 
 #include <config.h>
@@ -83,4 +103,3 @@ parse_opt (int key, char *arg, struct argp_state *state)
 }
 
 static struct argp argp = {options, parse_opt, args_doc, doc, 0, 0, 0};
-
