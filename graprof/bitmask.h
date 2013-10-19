23b12a8069298ef8b6949825475f42fe313461ea
Andreas Grapentin <andreas@grapentin.org>
2013-10-19 13:41:05 +0200

    added missing in-place documentation to bitmask.h

--- graprof/bitmask.h
+++ graprof/bitmask.h
@@ -20,6 +20,9 @@
 
 #pragma once
 
+/* this struct represents a simple bitmask of variable length
+ */
+
 struct bitmask
 {
   unsigned int *fields;
@@ -28,12 +31,66 @@ struct bitmask
 
 typedef struct bitmask bitmask;
 
+/* return a pointer to a newly allocated and zero initialized bitmask
+ * structure. Note that to avoid memory leakage, this structure has to be 
+ * freed by using bitmask_destroy.
+ *
+ * params:
+ *   width - the width of the bitmask in bits
+ *
+ * errors:
+ *   may fail and set errno for the same reasons as malloc
+ *
+ * returns:
+ *   a pointer to a bitmask structure on success, NULL otherwise
+ */
 bitmask *bitmask_create(unsigned int width);
 
+/* return a pointer to a newly allocated deep copy of a given bitmask. Note
+ * that to avoid memory leakage, this structure has to be freed by using
+ * bitmask_destroy.
+ *
+ * params:
+ *   b - a pointer to a bitmask structure
+ *
+ * errors:
+ *   may fail and set errno for the same reasons as malloc
+ *
+ * returns:
+ *   a pointer to a bitmask structure on success, NULL otherwise
+ */
 bitmask *bitmask_copy(bitmask *b);
 
+/* destroy a bitmask structure and free all associated memory
+ *
+ * params:
+ *   b - a pointer to a pointer to a bitmask structure
+ */
 void bitmask_destroy(bitmask **b);
 
+/* set a bit in a bitmask
+ *
+ * params:
+ *   b - a pointer to a bitmask structure
+ *   i - the index of the bit to set
+ *
+ * errors:
+ *   the behaviour is undefined if i is greater than or equal to the width of
+ *   the bitmask
+ */
 void bitmask_set(bitmask *b, unsigned int i);
 
+/* get a bit in a bitmask
+ *
+ * params:
+ *   b - a pointer to a bitmask structure
+ *   i - the index of the bit to get
+ *
+ * errors:
+ *   the behaviour is undefined if i is greater than or equal to the width of
+ *   the bitmask
+ *
+ * returns:
+ *   a positive integer if the specified bit is set, 0 otherwise
+ */
 int bitmask_get(bitmask *b, unsigned int i);
